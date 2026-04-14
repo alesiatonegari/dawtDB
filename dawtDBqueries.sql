b8727e5d5e2566036b791014ad5031aa59f8c491
alesiatonegari <158767896+alesiatonegari@users.noreply.github.com>
2026-04-13 22:41:59 -0500

    Added SQL queries for genre popularity and movie ratings

--- dawtDBqueries.sql
+++ dawtDBqueries.sql
@@ -26,3 +26,25 @@ SELECT
 FROM MOVIE m
 JOIN COUNTRY c ON m.country_id = c.country_id
 ORDER BY runtime DESC;
+
+-- 5. Most popular genre by total watches
+SELECT
+    g.genre_name,
+    COUNT(wl.log_id) AS total_watches
+FROM GENRE g
+JOIN MOVIE_GENRE mg ON g.genre_id  = mg.genre_id
+JOIN WATCHLOG   wl ON mg.movie_id = wl.movie_id
+JOIN MOVIE       m ON mg.movie_id = m.movie_id
+GROUP BY g.genre_id, g.genre_name
+ORDER BY total_watches DESC;
+
+-- 6. Average rating per movie with total watch count
+SELECT
+    m.title,
+    ROUND(AVG(r.rating_value), 2) AS avg_rating,
+    COUNT(wl.log_id)              AS total_watches
+FROM MOVIE m
+LEFT JOIN RATING   r  ON m.movie_id = r.movie_id
+LEFT JOIN WATCHLOG wl ON m.movie_id = wl.movie_id
+GROUP BY m.movie_id, m.title
+ORDER BY avg_rating DESC, total_watches DESC;
